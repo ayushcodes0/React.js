b1d35ec8d60e4432ffb76baa0d083f97da4aea78
ayushcodes0 <ayushsinghcodes@gmail.com>
2025-02-11 14:50:25 +0530

    updated spotify link

--- basics/src/App.jsx
+++ basics/src/App.jsx
@@ -1,7 +1,4 @@
 import React from 'react'
-import Header from './component/Header'
-import MainPart from './component/MainPart'
-import Footer from './component/Footer'
 import UsingUseState from './component/UsingUseState'
 
 
---
+++ basics/src/component/ShoppingList.jsx
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+
+const ShoppingList = () => {
+    const [item, setItem] = useState([]);
+    const [name, setName] = useState("");
+    const [quantity, setQuantity] = useState("");
+
+    const handleSubmit = (e)=>{
+        e.preventDefault();
+        if(!name || !quantity) return;
+        const newItem = {
+            name, quantity: parseInt(quantity)
+        }
+        setItem((prevItem) => [...prevItem, newItem]);
+        setName("");
+        setQuantity("");
+    }
+
+  return (
+    <div>
+        <h1>Shopping List</h1>
+      <form onSubmit={handleSubmit}>
+        <input type="text" placeholder='Enter name' value={name} onChange={e=>setName(e.target.value)} />
+        <input type="number" placeholder='Enter quantity' value={quantity} onChange={e=>setName(e.target.value)} />
+        <button type='submit'>Add Item</button>
+      </form> 
+      <ul>
+        {item.map((obj,index)=>{
+            return(
+                <li key={index}>
+                    {obj.name}, Quantity: {obj.quantity}
+                </li>
+            )
+        })}
+      </ul>
+    </div>
+  )
+}
+
+export default ShoppingList
